d5ecc76f67c550379f2073690baf8d4e6f987e42
jazzTheJackRabbit <amogh.91@gmail.com>
2017-09-23 19:13:25 -0700

    script changes for GPU instance

--- unzip_data_and_train.sh
+++ unzip_data_and_train.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 unzip data.zip
+. activate carnd-term1
 python3 train.py
--- upload_data.sh
+++ upload_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 zip -r ./data.zip data
-scp data.zip carnd@carnd:/sdc_project3/
+scp data.zip carnd@carnd:~/sdc_project3/data.zip
