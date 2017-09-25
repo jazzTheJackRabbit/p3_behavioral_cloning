d906eab90c762189c8669695aa761031b61b05dc
Amogh Param <amogh.91@gmail.com>
2017-09-25 15:42:49 -0700

    changing to bootstrap script

--- bootstrap.py
+++ bootstrap.py
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# Create virtualenv
+virtualenv ./ -p python3
+
 # Download Simulator
 wget https://d17h27t6h515a5.cloudfront.net/topher/2017/February/58ae4594_mac-sim.app/mac-sim.app.zip
